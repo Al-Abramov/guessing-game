ce6fe53990fe40bd483ea4af2a8893624d991165
Alex <x3m.f@mail.ru>
2021-08-10 11:16:16 +0400

    feat: implement class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+       this.attempt = Math.ceil((this.min + this.max)/2);
+       return this.attempt
     }
 
     lower() {
-
+        this.max = this.attempt
     }
 
     greater() {
-
+        this.min = this.attempt
     }
 }
 
